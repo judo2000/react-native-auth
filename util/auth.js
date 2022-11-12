31822215b8f94a62161409902c86510cc5837bf3
Stephen Moore <judo2000@comcast.net>
2022-11-12 09:23:09 -0700

    Create login function

--- util/auth.js
+++ util/auth.js
@@ -2,13 +2,22 @@ import axios from 'axios';
 
 const API_KEY = 'AIzaSyBuxXZqU41NUS7vSgA3IoHSX_r4myv7Ct0';
 
+export const authenticate = async (mode, email, password) => {
+  const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
+
+  const response = await axios.post(url, {
+    email,
+    password,
+    returnSecureToken: true,
+  });
+
+  console.log(response.data);
+};
+
 export const createUser = async (email, password) => {
-  const response = await axios.post(
-    'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + API_KEY,
-    {
-      email,
-      password,
-      returnSecureToken: true,
-    }
-  );
+  await authenticate('signUp', email, password);
+};
+
+export const login = async (email, password) => {
+  await authenticate('signInWithPassword', email, password);
 };
